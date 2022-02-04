878145b34b7a3a0dcdcb0cbed0e6586255d8cad6
Bagus Setiadi <bsetiadi2605@gmail.com>
2022-02-04 16:17:42 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="connectBluetooth">Haloooo</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      printCharacteristic : null,
+      zpl : "Hallo Bagus"
+    }
+  },
+  methods: {
+    connectBluetooth(){
+      navigator.bluetooth.requestDevice({
+        filters: [{
+                services: ['000018f0-0000-1000-8000-00805f9b34fb']
+              }]
+      })
+      .then(device => {
+        console.log('> Found ' + device.name);
+        console.log('Connecting to GATT Server...',device);
+        return device.gatt.connect();
+      })
+      .then(server => server.getPrimaryService("000018f0-0000-1000-8000-00805f9b34fb"))
+      .then(service => service.getCharacteristic("00002af1-0000-1000-8000-00805f9b34fb"))
+      .then(characteristic => {
+        console.log('Characteristic', characteristic);
+        // Cache the characteristic
+        this.printCharacteristic = characteristic;
+        // this.sendTextData()
+
+        // comment code di bawah ini jika ingin menggunakan this.sendTextData
+        var maxChunk = 300;
+        var j = 0;
+
+        if ( this.zpl.length > maxChunk ) {
+          for ( var i = 0; i < this.zpl.length; i += maxChunk ) {
+            var subStr;
+            if ( i + maxChunk <= this.zpl.length ) {
+              subStr = this.zpl.substring(i, i + maxChunk);
+
+            } else {
+              subStr = this.zpl.substring(i, this.zpl.length);
+            }
+
+            setTimeout(this.writeStrToCharacteristic, 250 * j, subStr);
+            j++;
+          }
+        } else {
+          this.writeStrToCharacteristic(this.zpl);
+        }
+      })
+      .catch(error => { console.error(error); });
+    },
+    sendPrinterData() {
+          // Print an image followed by the text
+      this.writeStrToCharacteristic('Halo Bagus')
+      .then(result => {
+        console.log('PRINT SUCCESS', result)
+      })
+      .catch(this.handleError);
+    },
+    handleError(error) {
+      console.log(error);
+      this.printCharacteristic = null;
+    },
+    sendTextData() {
+      // Get the bytes for the text
+      let encoder = new TextEncoder("utf-8");
+      // Add line feed + carriage return chars to text
+      let text = encoder.encode('Halo' + '\u000A\u000D');
+      console.log('text',text)
+      return this.printCharacteristic.writeValueWithoutResponse(text).then(() => {
+        console.log('Write done.');
+      });
+    },
+    writeStrToCharacteristic (str) {
+      let buffer = new ArrayBuffer(str.length);
+      let dataView = new DataView(buffer);
+      for (var i = 0; i <str.length; i++) {
+        dataView.setUint8( i, str.charAt(i).charCodeAt() );
+      }
+      console.log('accessing the device');
+      return this.printCharacteristic.writeValue(buffer);
+    }
   }
 }
 </script>
